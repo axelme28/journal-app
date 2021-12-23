f4f269f5b4615bf49ce49dae36368ad70b044afc
Axel Mendez <71779664+axelme28@users.noreply.github.com>
2021-12-22 21:14:28 -0600

    add regosterScreen

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const RegisterScreen = () => {
     return (
-        <div>
-            <h1>
-                Register Screen
-            </h1>
-        </div>
-    )
-}
+        <>
+            <form>
+                <h3
+                    className="auth__title"
+                    style={{ display: "flex", justifyContent: "center" }}
+                >
+                    Register
+                </h3>
+
+                <input
+                    type="text"
+                    name="name"
+                    className="auth__input"
+                    placeholder="Name"
+                />
+
+                <input
+                    type="text"
+                    name="email"
+                    placeholder="Email"
+                    className="auth__input"
+                    autoComplete="off"
+                />
+
+                <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="auth__input"
+                />
+
+                <input
+                    type="password"
+                    name="confirmPassword"
+                    placeholder="Confirm Password"
+                    className="auth__input"
+                />
+
+                <button
+                    type="submit"
+                    className="btn btn-primary btn-block mb-5"
+                >
+                    Register
+                </button>
+                <Link to="/auth/login" className="link">
+                    already have an account?
+                </Link>
+            </form>
+        </>
+    );
+};
